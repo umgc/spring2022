0f7389910c23e74ef11a94f5fb3a20e801a4b0e6
The-Jewell <jjewell1989@gmail.com>
2022-02-18 22:11:02 -0600

    Bottom UX/Navigation fixes
    #1 fixed bottom nav icon to set active icon to white and non active to black
    #2 updated titles of pages in menu bar(notes- previously showed checklist, settings- previously showed setting)
    3: added functionality that turns off mic pulse animation upon clicking another tab(previously would continue to pulse even if you clicked onto another page unless you clicked the mic again to disable it)

--- lib/Observables/ScreenNavigator.dart
+++ lib/Observables/ScreenNavigator.dart
@@ -1,38 +1,49 @@
 import 'package:mobx/mobx.dart';
 import 'package:untitled3/Utility/Constant.dart';
+
 part 'ScreenNavigator.g.dart';
+
 class MainNavObserver = _AbstractMainNavObserver with _$MainNavObserver;
 
 abstract class _AbstractMainNavObserver with Store {
-
   @observable
   dynamic currentScreen;
 
   @observable
-  String screenTitle ="";
+  String screenTitle = "";
 
   @observable
   dynamic focusedNavBtn = 0;
 
   @action
-  void changeScreen(dynamic screen){
+  void changeScreen(dynamic screen) {
     print("Screen changed to: $screen");
     currentScreen = screen;
     focusedNavBtn = -1;
+    // if (screen == MAIN_SCREENS.MENU) {
+    //   currentScreen = screen;
+    //   focusedNavBtn = 0;
+    // } else if (screen == MAIN_SCREENS.HOME) {
+    //   currentScreen = screen;
+    //   focusedNavBtn = 1;
+    // } else if (screen == MENU_SCREENS.HELP) {
+    //   currentScreen = screen;
+    //   focusedNavBtn = 2;
+    // }
   }
 
   @action
-  void setTitle(String title){
-    print("Change Screen tittle to: "+ title);
+  void setTitle(String title) {
+    print("Change Screen tittle to: " + title);
     screenTitle = title;
-    
   }
 
   @action
-  void setFocusedBtn(dynamic focusedBtn){
+  void setFocusedBtn(dynamic focusedBtn) {
     focusedNavBtn = focusedBtn;
-    if(focusedBtn == 2) currentScreen = MAIN_SCREENS.NOTE;
-    if(focusedBtn == 0) currentScreen = MAIN_SCREENS.MENU;
-  }
 
+    if (focusedBtn == 2) currentScreen = MENU_SCREENS.HELP;
+    if (focusedBtn == 0) currentScreen = MAIN_SCREENS.MENU;
+    if (focusedBtn == 1) currentScreen = MAIN_SCREENS.HOME;
+  }
 }
