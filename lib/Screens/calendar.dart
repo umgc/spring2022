a2a842b3cffb99a1a8090fe9527755ff9b8b30b4
Brian Avadikian <Bavadikian@gmail.com>
2022-02-19 19:29:17 -0500

    Linked the focusable Month, Week, Day Buttons to the actual state of the calendar.

--- lib/Screens/calendar.dart
+++ lib/Screens/calendar.dart
@@ -12,9 +12,14 @@ import 'package:provider/provider.dart';
 import 'package:untitled3/generated/i18n.dart';
 
 final viewCalendarScaffoldKey = GlobalKey<ScaffoldState>();
+
+//Current button and View State attributes
 bool _weekHasBeenPressed = false;
 bool _monthHasBeenPressed = true;
 bool _dayHasBeenPressed = false;
+
+bool _calendarIsVisable = true;
+var _calendarFormat = CalendarFormat.month;
 
 class Calendar extends StatefulWidget {
   @override
@@ -35,6 +40,7 @@ class CalendarState extends State<Calendar> {
     final settingObserver = Provider.of<SettingObserver>(context);
 
     calendarObserver.setNoteObserver(noteObserver);
+    calendarObserver.calendarFormat = _calendarFormat;
 
     return Scaffold(
       body: SingleChildScrollView(
@@ -74,6 +80,8 @@ class CalendarState extends State<Calendar> {
                       _weekHasBeenPressed = true;
                       _dayHasBeenPressed = false;
                       _monthHasBeenPressed = false;
+                      _calendarIsVisable = true;
+                      _calendarFormat = CalendarFormat.week;
                     })
                   },
                 ),
@@ -92,7 +100,7 @@ class CalendarState extends State<Calendar> {
                 TextButton(
                   style: TextButton.styleFrom(
                     primary:
-                    _monthHasBeenPressed ? Colors.black : Colors.blueGrey,
+                        _monthHasBeenPressed ? Colors.black : Colors.blueGrey,
                     textStyle: TextStyle(
                       fontWeight: _monthHasBeenPressed
                           ? FontWeight.bold
@@ -113,6 +121,9 @@ class CalendarState extends State<Calendar> {
                       _monthHasBeenPressed = true;
                       _weekHasBeenPressed = false;
                       _dayHasBeenPressed = false;
+                      _calendarIsVisable = true;
+                      _calendarFormat = CalendarFormat.month;
+
                     })
                   },
                 ),
@@ -131,7 +142,7 @@ class CalendarState extends State<Calendar> {
                 TextButton(
                   style: TextButton.styleFrom(
                     primary:
-                    _dayHasBeenPressed ? Colors.black : Colors.blueGrey,
+                        _dayHasBeenPressed ? Colors.black : Colors.blueGrey,
                     textStyle: TextStyle(
                       fontWeight: _dayHasBeenPressed
                           ? FontWeight.bold
@@ -152,59 +163,60 @@ class CalendarState extends State<Calendar> {
                       _dayHasBeenPressed = true;
                       _monthHasBeenPressed = false;
                       _weekHasBeenPressed = false;
+                      _calendarIsVisable = false;
                     })
                   },
                 ),
               ],
             ),
-            TableCalendar(
-              focusedDay: DateTime.now(),
-              locale: settingObserver.userSettings.locale.languageCode,
-              firstDay:
-                  DateTime.parse("2022-02-15"), //Date of the oldest past event
-              lastDay: DateTime.parse("2023-02-27"), //Date of the last event
-              selectedDayPredicate: (day) {
-                return isSameDay(calendarObserver.selectedDay, day);
-              },
+            Visibility(
+              visible: _calendarIsVisable,
+              child:
+              TableCalendar(
+                focusedDay: DateTime.now(),
+                calendarFormat: calendarObserver.calendarFormat,
+                locale: settingObserver.userSettings.locale.languageCode,
+                firstDay: DateTime.parse(
+                    "2022-02-15"), //Date of the oldest past event
+                lastDay: DateTime.parse("2023-02-27"), //Date of the last event
+                selectedDayPredicate: (day) {
+                  return isSameDay(calendarObserver.selectedDay, day);
+                },
 
-              calendarFormat: calendarObserver.calendarFormat,
-              eventLoader: (DateTime day) {
-                return calendarObserver
-                    .loadEventsOfSelectedDay(day.toString().split(" ")[0]);
-              },
-              onFormatChanged: (format) {
-                calendarObserver.changeFormat(format);
-              },
-              onDaySelected: (selectedDay, focusDay) {
-                print("onDaySelected selectedDay: $selectedDay");
-                //exctract the date portion
-                calendarObserver.setSelectedDay(selectedDay);
-                calendarObserver.loadEventsOfSelectedDay(
-                    selectedDay.toString().split(" ")[0]);
+                eventLoader: (DateTime day) {
+                  return calendarObserver
+                      .loadEventsOfSelectedDay(day.toString().split(" ")[0]);
+                },
+                onFormatChanged: (format) {
+                  calendarObserver.changeFormat(format);
+                },
+                onDaySelected: (selectedDay, focusDay) {
+                  print("onDaySelected selectedDay: $selectedDay");
+                  //exctract the date portion
+                  calendarObserver.setSelectedDay(selectedDay);
+                  calendarObserver.loadEventsOfSelectedDay(
+                      selectedDay.toString().split(" ")[0]);
 
-                (context as Element).reassemble();
-              },
-              onPageChanged: (focusedDay) {},
-              calendarStyle: CalendarStyle(
-                  selectedDecoration: BoxDecoration(
-                    color: Colors.pink,
-                    shape: BoxShape.circle,
-                  ),
-                  //selectedTextStyle: TextStyle(),
-                  //todayDecoration: Colors.orange,
-                  todayDecoration: BoxDecoration(
-                    color: Colors.blueAccent,
-                    shape: BoxShape.circle,
-                  ),
-                  //OnDaySelected: Theme.of(context).primaryColor,
-                  selectedTextStyle: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18.0,
-                      color: Colors.white)),
-            ),
-            ElevatedButton(
-              child: Text(I18n.of(context)!.clearSelection),
-              onPressed: () {},
+                  (context as Element).reassemble();
+                },
+                onPageChanged: (focusedDay) {},
+                calendarStyle: CalendarStyle(
+                    selectedDecoration: BoxDecoration(
+                      color: Colors.pink,
+                      shape: BoxShape.circle,
+                    ),
+                    //selectedTextStyle: TextStyle(),
+                    //todayDecoration: Colors.orange,
+                    todayDecoration: BoxDecoration(
+                      color: Colors.blueAccent,
+                      shape: BoxShape.circle,
+                    ),
+                    //OnDaySelected: Theme.of(context).primaryColor,
+                    selectedTextStyle: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18.0,
+                        color: Colors.white)),
+              ),
             ),
           ],
         ),
