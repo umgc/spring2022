884497a184d298b0700d4e7d6b39dfeea69f5f88
Brian Avadikian <Bavadikian@gmail.com>
2022-02-26 19:19:13 -0500

    Added Single Child Scroll iew

--- lib/Screens/HomePage.dart
+++ lib/Screens/HomePage.dart
@@ -35,138 +35,140 @@ class _HomePageState extends State<HomePage>
         backgroundColor: Colors.white,
         body: Center(
 
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: 100.0),
-              Text('MemorEZ', style: TextStyle(color: Color(0xFF0D47A1), fontSize: 40, fontWeight: FontWeight.w900)),
-              SizedBox(height: 20.0),
-              Text('Spend less time remembering and more time doing', style: TextStyle(fontSize: 14, fontWeight: FontWeight.w900)),
-              SizedBox(height: 60.0),
-              ClipRRect(
-                borderRadius: BorderRadius.circular(15),
-                child: Stack(
-                  children: <Widget>[
-                    Positioned.fill(
-                      child: Container(
-                        decoration: const BoxDecoration(
-                              color: Color(0xFF0D47A1),
-                          // gradient: LinearGradient(
-                          //   colors: <Color>[
-                          //     Color(0xFF0D47A1),
-                          //     Color(0xFF1976D2),
-                          //     Color(0xFF42A5F5),
-                          //   ],
-                          // ),
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: 100.0),
+                Text('MemorEZ', style: TextStyle(color: Color(0xFF0D47A1), fontSize: 40, fontWeight: FontWeight.w900)),
+                SizedBox(height: 20.0),
+                Text('Spend less time remembering and more time doing', style: TextStyle(fontSize: 14, fontWeight: FontWeight.w900)),
+                SizedBox(height: 60.0),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(15),
+                  child: Stack(
+                    children: <Widget>[
+                      Positioned.fill(
+                        child: Container(
+                          decoration: const BoxDecoration(
+                                color: Color(0xFF0D47A1),
+                            // gradient: LinearGradient(
+                            //   colors: <Color>[
+                            //     Color(0xFF0D47A1),
+                            //     Color(0xFF1976D2),
+                            //     Color(0xFF42A5F5),
+                            //   ],
+                            // ),
+                          ),
                         ),
                       ),
-                    ),
 
-                    TextButton.icon(
-                      icon: const Icon(Icons.person),
-                      label: const Text(' PATIENT '),
-                      style: TextButton.styleFrom(
-                        padding : const EdgeInsets.only(left: 90, right: 90.0),
-                        primary: Colors.white,
-                        textStyle: const TextStyle(fontSize: 20),
-                      ),
-                      onPressed: () {
-                        Navigator.push(context,
-                          MaterialPageRoute(builder: (context){
-                            return MainNavigator();
-                          }),
-                        );
-                      },
-                    )
-                    // TextButton(
-                    //   style: TextButton.styleFrom(
-                    //     padding: const EdgeInsets.all(16.0),
-                    //     primary: Colors.white,
-                    //     textStyle: const TextStyle(fontSize: 20),
-                    //   ),
-                    //   onPressed: () {
-                    //     Navigator.push(context,
-                    //       MaterialPageRoute(builder: (context){
-                    //         return LoginForm();
-                    //       }),
-                    //     );
-                    //   },
-                    //   child: const Text('  Admin Mode  '),
-                    // ),
-
-
-                  ],
-                ),
-              ),
-              SizedBox(height: 20.0),
-              ClipRRect(
-                borderRadius: BorderRadius.circular(15),
-
-                child: Stack(
-                  children: <Widget>[
-                    Positioned.fill(
-                      child: Container(
-
-                        decoration: const BoxDecoration(
-
-                          color: Color(0xFF0D47A1),
-                          // gradient: LinearGradient(
-                          //   colors: <Color>[
-                          //     Color(0xFF0D47A1),
-                          //     Color(0xFF1976D2),
-                          //     Color(0xFF42A5F5),
-                          //   ],
-                          // ),
-                        ),
-                      ),
-                    ),
-                    TextButton.icon(
-
-                      icon: const Icon(Icons.medical_services),
-                      label: const Text('CAREGIVER'),
+                      TextButton.icon(
+                        icon: const Icon(Icons.person),
+                        label: const Text(' PATIENT '),
                         style: TextButton.styleFrom(
-                          padding : const EdgeInsets.only(left: 80, right: 80.0),
-                          // padding: const EdgeInsets.all(15.0),
+                          padding : const EdgeInsets.only(left: 90, right: 90.0),
                           primary: Colors.white,
                           textStyle: const TextStyle(fontSize: 20),
                         ),
                         onPressed: () {
                           Navigator.push(context,
                             MaterialPageRoute(builder: (context){
-                              return LoginForm();
+                              return MainNavigator();
                             }),
                           );
                         },
-                    ),
-
-                    // TextButton(
-                    //
-                    //   style: TextButton.styleFrom(
-                    //     padding: const EdgeInsets.all(16.0),
-                    //     primary: Colors.white,
-                    //     textStyle: const TextStyle(fontSize: 20),
-                    //   ),
-                    //   onPressed: () {
-                    //     Navigator.push(context,
-                    //       MaterialPageRoute(builder: (context){
-                    //         return MainNavigator();
-                    //       }),
-                    //     );
-                    //   },
-                    //   child: const Text('Normal Access'),
-                    // ),
+                      )
+                      // TextButton(
+                      //   style: TextButton.styleFrom(
+                      //     padding: const EdgeInsets.all(16.0),
+                      //     primary: Colors.white,
+                      //     textStyle: const TextStyle(fontSize: 20),
+                      //   ),
+                      //   onPressed: () {
+                      //     Navigator.push(context,
+                      //       MaterialPageRoute(builder: (context){
+                      //         return LoginForm();
+                      //       }),
+                      //     );
+                      //   },
+                      //   child: const Text('  Admin Mode  '),
+                      // ),
 
 
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-              SizedBox(height: 50.0),
-              Image.asset(
-                "assets/images/caregiver_patient_image.png",
-                height: 400.0,
-                width: 400.0,
-              ),
+                SizedBox(height: 20.0),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(15),
 
-            ],
+                  child: Stack(
+                    children: <Widget>[
+                      Positioned.fill(
+                        child: Container(
+
+                          decoration: const BoxDecoration(
+
+                            color: Color(0xFF0D47A1),
+                            // gradient: LinearGradient(
+                            //   colors: <Color>[
+                            //     Color(0xFF0D47A1),
+                            //     Color(0xFF1976D2),
+                            //     Color(0xFF42A5F5),
+                            //   ],
+                            // ),
+                          ),
+                        ),
+                      ),
+                      TextButton.icon(
+
+                        icon: const Icon(Icons.medical_services),
+                        label: const Text('CAREGIVER'),
+                          style: TextButton.styleFrom(
+                            padding : const EdgeInsets.only(left: 80, right: 80.0),
+                            // padding: const EdgeInsets.all(15.0),
+                            primary: Colors.white,
+                            textStyle: const TextStyle(fontSize: 20),
+                          ),
+                          onPressed: () {
+                            Navigator.push(context,
+                              MaterialPageRoute(builder: (context){
+                                return LoginForm();
+                              }),
+                            );
+                          },
+                      ),
+
+                      // TextButton(
+                      //
+                      //   style: TextButton.styleFrom(
+                      //     padding: const EdgeInsets.all(16.0),
+                      //     primary: Colors.white,
+                      //     textStyle: const TextStyle(fontSize: 20),
+                      //   ),
+                      //   onPressed: () {
+                      //     Navigator.push(context,
+                      //       MaterialPageRoute(builder: (context){
+                      //         return MainNavigator();
+                      //       }),
+                      //     );
+                      //   },
+                      //   child: const Text('Normal Access'),
+                      // ),
+
+
+                    ],
+                  ),
+                ),
+                SizedBox(height: 50.0),
+                Image.asset(
+                  "assets/images/caregiver_patient_image.png",
+                  height: 400.0,
+                  width: 400.0,
+                ),
+
+              ],
+            ),
           ),
         ),
 
